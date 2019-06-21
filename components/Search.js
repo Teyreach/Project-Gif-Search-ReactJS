7cbf5aa41907879909a6c2354bd34f02541d3126
Robert Koszela <32070507+Teyreach@users.noreply.github.com>
2019-06-21 16:12:16 +0000

    Update Search.js

--- components/Search.js
+++ components/Search.js
@@ -21,7 +21,7 @@ Search = React.createClass({
     },
 
 
-    render: function() {
+     render: function() {
       var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
       return <input
